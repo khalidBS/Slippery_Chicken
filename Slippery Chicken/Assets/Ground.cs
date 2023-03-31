40370759e100f6f23ba0fb06f4d394f83616a4b5
Kahlid <kha8lid@hotmail.com>
2023-03-31 17:06:41 +0300

    Version 1
    - Slippery Movement
    
    - Eggs Breaking

---
+++ Slippery Chicken/Assets/Ground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground: MonoBehaviour
+{
+    public bool grounded;
+
+    private void OnTriggerStay2D(Collider2D collider) {
+        grounded = collider != null;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        grounded = false;
+
+    }
+}
---
+++ Slippery Chicken/Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    bool grounded;
+
+    [SerializeField]
+    Transform GroundCheck;
+
+
+    public GameObject broken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.Find("GroundCheck").GetComponent<Ground>().grounded && grounded)
+        {
+            Instantiate(broken,rb2d.transform);
+            grounded = false;
+        }
+
+        if (Input.GetKey("right")) {
+
+            grounded = true;
+            if (rb2d.angularVelocity <300f)
+                rb2d.angularVelocity = -30f + rb2d.angularVelocity;
+            else
+                rb2d.angularVelocity = -30f;
+        }
+        if (Input.GetKey("left"))
+            rb2d.angularVelocity = 30f + rb2d.angularVelocity;
+    }
+}
